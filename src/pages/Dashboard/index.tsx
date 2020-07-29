c7ab6679eaa5b6fe3434c55d4d58348691bbfa38
Flavio Ever <flavio.ever@outlook.com>
2020-07-29 01:02:42 -0300

    Routes

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+/**
+ * Este formato 'e mais interessante, pois consigo passar
+ * o formato da variavel (funcao) do que usar function xxx() {}
+ */
+const Dashboard: React.FC = () => {
+  return <h1>Dashboard</h1>;
+};
+
+export default Dashboard;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+/**
+ * Este formato 'e mais interessante, pois consigo passar
+ * o formato da variavel (funcao) do que usar function xxx() {}
+ */
+const Repository: React.FC = () => {
+  return <h1>Repository</h1>;
+};
+
+export default Repository;
