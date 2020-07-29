e1a7b3d52c87183a2cb52163b384aa1868a434f3
Flavio Ever <flavio.ever@outlook.com>
2020-07-29 01:40:20 -0300

    Styled Components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-/**
- * Este formato 'e mais interessante, pois consigo passar
- * o formato da variavel (funcao) do que usar function xxx() {}
- */
+import { Title } from './styles';
+
 const Dashboard: React.FC = () => {
-  return <h1>Dashboard</h1>;
+  return <Title>Explore Repositorios no Github</Title>;
 };
 
 export default Dashboard;
