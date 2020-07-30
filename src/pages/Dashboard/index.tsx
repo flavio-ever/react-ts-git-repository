5a4d19cbee43f90c080c8d95137be20ace799b4b
Flavio Ever <flavio.silva@mazzatech.com.br>
2020-07-30 00:13:33 -0300

    Styled Items

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logo from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore Repositorios no Github</Title>;
+  return (
+    <>
+      <img src={logo} alt="" />
+      <Title>Explore Repositorios no Github</Title>
+
+      <Form action="">
+        <input type="text" placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="a">
+          <img
+            src="https://avatars3.githubusercontent.com/u/22870639?s=400&u=a67c5d214c08df899bd8b55f6b8ba62e3af38578&v=4"
+            alt="Flavio Ev3r"
+          />
+          <div>
+            <strong>flavio.ever/unform</strong>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="a">
+          <img
+            src="https://avatars3.githubusercontent.com/u/22870639?s=400&u=a67c5d214c08df899bd8b55f6b8ba62e3af38578&v=4"
+            alt="Flavio Ev3r"
+          />
+          <div>
+            <strong>flavio.ever/unform</strong>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="a">
+          <img
+            src="https://avatars3.githubusercontent.com/u/22870639?s=400&u=a67c5d214c08df899bd8b55f6b8ba62e3af38578&v=4"
+            alt="Flavio Ev3r"
+          />
+          <div>
+            <strong>flavio.ever/unform</strong>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
