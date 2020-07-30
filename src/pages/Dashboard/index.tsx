743dc43c6a7643f3a7516597556615e83099b9dc
Flavio Ever <flavio.silva@mazzatech.com.br>
2020-07-30 00:45:14 -0300

    Listando repositorios

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,58 +1,67 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
   return (
     <>
       <img src={logo} alt="" />
       <Title>Explore Repositorios no Github</Title>
 
-      <Form action="">
-        <input type="text" placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
+          type="text"
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="a">
-          <img
-            src="https://avatars3.githubusercontent.com/u/22870639?s=400&u=a67c5d214c08df899bd8b55f6b8ba62e3af38578&v=4"
-            alt="Flavio Ev3r"
-          />
-          <div>
-            <strong>flavio.ever/unform</strong>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
-          </div>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="a">
-          <img
-            src="https://avatars3.githubusercontent.com/u/22870639?s=400&u=a67c5d214c08df899bd8b55f6b8ba62e3af38578&v=4"
-            alt="Flavio Ev3r"
-          />
-          <div>
-            <strong>flavio.ever/unform</strong>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="a">
-          <img
-            src="https://avatars3.githubusercontent.com/u/22870639?s=400&u=a67c5d214c08df899bd8b55f6b8ba62e3af38578&v=4"
-            alt="Flavio Ev3r"
-          />
-          <div>
-            <strong>flavio.ever/unform</strong>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
