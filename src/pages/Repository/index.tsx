2512eff1275fd77d287435f341c02872369e171c
Flavio Ever <flavio.silva@mazzatech.com.br>
2020-07-30 21:36:44 -0300

    Repository Styles

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,11 +1,66 @@
 import React from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
-/**
- * Este formato 'e mais interessante, pois consigo passar
- * o formato da variavel (funcao) do que usar function xxx() {}
- */
+import logo from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
+
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  return (
+    <>
+      <Header>
+        <img src={logo} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars3.githubusercontent.com/u/22870639?s=400&u=a67c5d214c08df899bd8b55f6b8ba62e3af38578&v=4"
+            alt="X"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>100</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>44</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>32</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="dsadsa">
+          <div>
+            <strong>AAAA</strong>
+            <p>BBBBB</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
